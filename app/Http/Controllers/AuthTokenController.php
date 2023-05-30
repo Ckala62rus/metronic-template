b2c2327e630bfb35e82a0c0ad94a47f46a2d0a05
Овчинников Евгений <agr.akyla@mail.ru>
2023-05-30 19:25:50 +0300

    add swagger

--- app/Http/Controllers/AuthTokenController.php
+++ app/Http/Controllers/AuthTokenController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -16,6 +15,75 @@ use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 class AuthTokenController extends BaseController
 {
     /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     summary="Login (Get bearer token)",
+     *     tags={"Auth"},
+     *     security={
+     *      {"passport": {}},
+     *     },
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string"
+     *                 ),
+     *                 example={"email": "admin@mail.ru", "password": "123123"}
+     *             )
+     *         )
+     *     ),
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="Authorization success!",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="status", type="boolean", example="true"),
+     *              @OA\Property(property="message", type="string", example="User logged-in!"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="object",
+     *                  @OA\Property(
+     *                      property="auth",
+     *                      type="object",
+     *                      @OA\Property(property="token", type="string", example="ikH5Kp+Urr8yzekJutELi9Te0s/Ln4p+42uwe4CIDA0="),
+     *                      @OA\Property(property="name", type="string", example="admin"),
+     *                  ),
+     *              ),
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=401,
+     *          description="Aauthentication failed!",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="status", type="boolean", example="false"),
+     *              @OA\Property(property="message", type="string", example="Unauthorised"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="object",
+     *                  @OA\Property(
+     *                      property="auth",
+     *                      type="null",
+     *                      example="null"
+     *                  ),
+     *              ),
+     *          )
+     *      ),
+     *
+     * ),
+     *
+     * @OAS\SecurityScheme(
+     *      securityScheme="bearerAuth",
+     *      type="http",
+     *      scheme="bearer"
+     * )
+     *
      * @param Request $request
      * @return JsonResponse
      */
@@ -96,6 +164,55 @@ class AuthTokenController extends BaseController
     }
 
     /**
+     * @OA\Get(
+     *     path="/api/me",
+     *     summary="Get current auth user model",
+     *     tags={"Auth"},
+     *     security={{"bearerAuth":{}}},
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="Ok",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="status", type="boolean", example="true"),
+     *              @OA\Property(property="message", type="string", example="Current Auth user"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="object",
+     *                  @OA\Property(
+     *                      property="user",
+     *                      type="object",
+     *                          @OA\Property(property="id", type="integer", example="1"),
+     *                          @OA\Property(property="name", type="string", example="admin"),
+     *                          @OA\Property(property="email", type="string", example="admin@mail.ru"),
+     *                          @OA\Property(property="email_verified_at", type="null", example="null"),
+     *                          @OA\Property(property="created_at", type="string", example="2023-02-28T06:05:41.000000Z"),
+     *                          @OA\Property(property="updated_at", type="string", example="2023-02-28T06:05:41.000000Z"),
+     *                  ),
+     *              ),
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthntication",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="status", type="boolean", example="false"),
+     *              @OA\Property(property="message", type="string", example="Current Auth user"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="object",
+     *                  @OA\Property(
+     *                      property="user",
+     *                      type="null",
+     *                      example="null"
+     *                  ),
+     *              ),
+     *          )
+     *      ),
+     *
+     * )
+     *
      * @return JsonResponse
      */
     public function me(): JsonResponse
